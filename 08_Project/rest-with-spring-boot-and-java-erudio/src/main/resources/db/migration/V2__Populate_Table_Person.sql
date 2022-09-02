fd2b079ded0d0707cbfd8e903afdd7389b9d3fb6
Mardonio Rodrigues <mardoniorodrigues11@gmail.com>
2022-09-02 16:52:50 -0300

    Flyway suport

---
+++ 08_Project/rest-with-spring-boot-and-java-erudio/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,6 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES 
+	(1,'São Paulo','Airton','Male','Senna'),
+	(2,'Anchiano - Italy','Leonardo','Male','da Vince'),
+	(6,'Kentucky - US','Muhammad','Male','Ali'),
+	(8,'Mvezo - South Africa','Nelson','Male','Mandela'),
+	(9,'Mvezo - South Africa','Nelson','Male','Mandela');
