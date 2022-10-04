9a56dd9e50045a36e649dedb07cd0f4388898c18
Mardonio Rodrigues <mardoniorodrigues11@gmail.com>
2022-10-04 20:44:48 -0300

    Adicionado suporte ao Swagger

---
+++ 08_Project/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/services/BookServices.java
@@ -0,0 +1,78 @@
+package br.com.erudio.services;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.erudio.data.vo.v1.BookVO;
+import br.com.erudio.exceptions.RequiredObjectIsNullException;
+import br.com.erudio.exceptions.ResourceNotFoundException;
+import br.com.erudio.mapper.DozerMapper;
+import br.com.erudio.model.Book;
+import br.com.erudio.repositories.BookRepository;
+
+@Service
+public class BookServices {
+	
+	private Logger logger = Logger.getLogger(BookServices.class.getName());
+	
+	@Autowired
+	BookRepository repository;
+
+	public List<BookVO> findAll() {
+
+		logger.info("Finding all book!");
+
+		var books = DozerMapper.parseListObjects(repository.findAll(), BookVO.class);
+		return books;
+	}
+
+	public BookVO findById(Long id) {
+		
+		logger.info("Finding one book!");
+		
+		var entity = repository.findById(id)
+			.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+		var vo = DozerMapper.parseObject(entity, BookVO.class);
+		return vo;
+	}
+	
+	public BookVO create(BookVO book) {
+
+		if (book == null) throw new RequiredObjectIsNullException();
+		
+		logger.info("Creating one book!");
+		var entity = DozerMapper.parseObject(book, Book.class);
+		var vo =  DozerMapper.parseObject(repository.save(entity), BookVO.class);
+		return vo;
+	}
+	
+	public BookVO update(BookVO book) {
+
+		if (book == null) throw new RequiredObjectIsNullException();
+		
+		logger.info("Updating one book!");
+		
+		var entity = repository.findById(book.getKey())
+			.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+
+		entity.setAuthor(book.getAuthor());
+		entity.setLaunchDate(book.getLaunchDate());
+		entity.setPrice(book.getPrice());
+		entity.setTitle(book.getTitle());
+		
+		var vo =  DozerMapper.parseObject(repository.save(entity), BookVO.class);
+		return vo;
+	}
+	
+	public void delete(Long id) {
+		
+		logger.info("Deleting one book!");
+		
+		var entity = repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+		repository.delete(entity);
+	}
+}
